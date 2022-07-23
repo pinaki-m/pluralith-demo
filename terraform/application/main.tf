f918f650129af4776cdb02de150e8054c70704bc
Pinaki Mukherjee <pinaki@mukherjee.com>
2022-07-23 15:56:35 +1000

    Update

--- terraform/application/main.tf
+++ terraform/application/main.tf
@@ -90,14 +90,3 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
   }
 }
 
-# resource "aws_redshift_cluster" "example" {
-#   cluster_identifier = "tf-redshift-cluster"
-#   database_name      = "mydb"
-#   master_username    = "exampleuser"
-#   master_password    = "Seriously9ow?!"
-#   node_type          = "dc1.large"
-#   cluster_type       = "single-node"
-# }
-
-
-
