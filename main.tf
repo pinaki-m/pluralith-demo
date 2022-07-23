f145866e62a265f1d20e785f6f6ceebacbebee9b
Pinaki Mukherjee <pinaki@mukherjee.com>
2022-07-23 10:38:23 +1000

    Update

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "aws_redshift_cluster" "example" {
   cluster_identifier = "tf-redshift-cluster"
   database_name      = "mydb"
   master_username    = "exampleuser"
-  master_password    = "Seriously9ow!"
+  master_password    = "Seriously9ow?!"
   node_type          = "dc1.large"
   cluster_type       = "single-node"
 }
